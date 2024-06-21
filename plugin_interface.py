138f6502bca91345c0a4f61ed3061fdbf985ac95
Ralf Krümmel <ralf.kruemmel+python@outlook.de>
2024-06-21 13:23:11 +0200

    Delete plugin_interface.py

--- plugin_interface.py
+++
@@ -1,9 +0,0 @@
-class PluginInterface:
-    def __init__(self, ide):
-        self.ide = ide
-
-    def initialize(self):
-        raise NotImplementedError("Plugins must implement the initialize method")
-
-    def deinitialize(self):
-        raise NotImplementedError("Plugins must implement the deinitialize method")
